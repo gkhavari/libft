297464adab4d82ac5ad882a3d12da916934c497d
Giti <giti.khavari@gmail.com>
2025-03-22 01:32:24 +0100

    added forgotten memmove

--- libft.h
+++ libft.h
@@ -19,7 +19,7 @@ size_t	ft_strlen(const char *str);
 void	*ft_memset(void *str, int c, size_t n);
 void	ft_bzero(void *str, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-/*ft_memmove*/
+void	*ft_memmove(void *dest, const void *src, size_t len);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 int		ft_toupper(int c);
