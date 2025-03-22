8e5ed861c31b4ca7656c6f53112c068abb9459ab
Giti <giti.khavari@gmail.com>
2025-03-22 01:30:14 +0100

    added forgotten memmove function

---
+++ ft_memmove.c
@@ -0,0 +1,34 @@
+#include "libft.h"
+
+void	*reverse_copy(char *dest, char *src, size_t len)
+{
+	while (len > 0)
+	{
+		dest[len - 1] = src[len - 1];
+		len--;
+	}
+	return ((void *)dest);
+}
+
+void	*normal_copy(char *dest, char *src)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return ((void *)dest);
+}
+
+void	*ft_memmove(void *dest, void *src, size_t len)
+{
+	if (dest == NULL || src == NULL)
+		return (NULL);
+	if (dest > src)
+		return (reverse_copy((char *)dest, (char *)src, len));
+	else
+		return (normal_copy((char *)dest, (char *)src));
+}
